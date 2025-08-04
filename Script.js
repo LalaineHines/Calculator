1db75c82066c0b0e67347351c10de56b92a7c383
LalaineHines <lalainehines@gmail.com>
2025-08-03 18:43:03 -0700

    Added global variables, starting on numberStored func

---
+++ Script.js
@@ -0,0 +1,16 @@
+// Remember to console log test each function
+
+// Global variables - to link HTML elements
+const button = document.querySelectorAll('button'); // Selects all HTML button elements
+const display = document.querySelector('.number-on-screen'); // Selects specifically the number-on-screen class elements
+
+// Global variables - empty string elements that can be filled in later
+let numberStored = ''; // To store numbers
+let operatorStored = ''; // To store operator signs
+
+// Global variables - Javascript booleans
+// Everything with a value is true, everything without a variable is false
+let newInput = false;
+let didEquals = false;
+
+//
